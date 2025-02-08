7419a7fbcf60974a084883960cee5f499530cb20
Tihara <tiharaeg@gmail.com>
2025-02-08 22:58:58 +0530

    fix: correct server port option in WebSocket server setup and improve message logging

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const ws = require('ws');
-const server = new ws.Server({post: '3000'})
+const server = new ws.Server({port: '3000'})
 
 server.on('connection', socket => {
     socket.on('message', message => {
-        console.log(message)
+        const b = Buffer.from(message)
+        console.log(b.toString())
         socket.send(`${message}`)
     })
 })
