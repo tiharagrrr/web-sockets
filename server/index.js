543a8e9107cb8a88e57b20b4b6f2091d8dc1db3d
Tihara <tiharaeg@gmail.com>
2025-03-04 13:39:36 +0530

    feat: add Express server setup

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
-import {createServer} from 'http'
+import express from 'express'
 import { Server } from 'socket.io'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-const httpServer = createServer()
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-const io = new Server(httpServer, {
+const PORT = process.env.PORT || 3500
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+const expressServer = app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+
+const io = new Server(expressServer, {
     cors: {
         origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"],
     }
@@ -16,5 +27,3 @@ io.on('connection', socket => {
         console.log(data)
         io.emit('message', `${socket.id.substring(0,5)}: ${data}`)})
 })
-
-httpServer.listen(3500, () => console.log('listening on port 3500'))
